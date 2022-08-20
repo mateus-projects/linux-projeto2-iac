62df9a31a4e1d9f2076740b3e4a55b5dced7c681
Mateus Camargo <84161435+mateus-projects@users.noreply.github.com>
2022-08-20 22:47:23 +0000

    Initial commit

---
+++ script-iac2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Atualizando servidor"
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e copiando os arquivos da aplicacao..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unsip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
